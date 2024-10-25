af6e913d553f06e59395a2d3346e9f3a5d48c282
Imen Frigui <imen_frigui@outlook.com>
2024-10-26 00:09:42 +0100

    front_office

--- symptom_checker_application/views.py
+++ symptom_checker_application/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'front_office/index.html')
+
+def about(request):
+    return render(request, 'front_office/about.html')
+
+def contact(request):
+    return render(request, 'front_office/contact.html')
+
+def blog(request):
+    return render(request, 'front_office/blog.html')
+
