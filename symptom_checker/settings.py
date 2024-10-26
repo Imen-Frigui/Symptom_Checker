276a3f0760464cf5714bc2dd062f86ab1a2666ff
Imen Frigui <imen_frigui@outlook.com>
2024-10-26 12:58:01 +0100

    back_office

--- symptom_checker/settings.py
+++ symptom_checker/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'back_office/assets', BASE_DIR / 'front_office/assets']
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "back_office/assets",
+    BASE_DIR / "front_office/assets",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
