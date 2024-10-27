05a4b6ebcb68a36b77ab624c385c8c5516e86ec6
Marwen-Neji <78695522+Marwen-Neji@users.noreply.github.com>
2024-10-27 13:22:59 +0100

    crud+views

---
+++ Consultation/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Consultation(models.Model):
+    doctor_name = models.CharField(max_length=100)
+    patient_name = models.CharField(max_length=100)
+    consultation_date = models.DateTimeField()
+    notes = models.TextField()
+    # add more fields as needed
+
+    def __str__(self):
+        return f"{self.doctor_name} - {self.patient_name}"
+
+
---
+++ Consultation/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Consultation
+from .forms import ConsultationForm
+
+def consultation_list(request):
+    consultations = Consultation.objects.all()
+    return render(request, 'consultation/consultation_list.html', {'consultations': consultations})
+
+def consultation_detail(request, pk):
+    consultation = get_object_or_404(Consultation, pk=pk)
+    return render(request, 'consultation/consultation_detail.html', {'consultation': consultation})
+
+def consultation_create(request):
+    if request.method == 'POST':
+        form = ConsultationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('consultation_list')
+    else:
+        form = ConsultationForm()
+    return render(request, 'consultation/consultation_form.html', {'form': form})
+
+def consultation_update(request, pk):
+    consultation = get_object_or_404(Consultation, pk=pk)
+    if request.method == 'POST':
+        form = ConsultationForm(request.POST, instance=consultation)
+        if form.is_valid():
+            form.save()
+            return redirect('consultation_list')
+    else:
+        form = ConsultationForm(instance=consultation)
+    return render(request, 'consultation/consultation_form.html', {'form': form})
+
+def consultation_delete(request, pk):
+    consultation = get_object_or_404(Consultation, pk=pk)
+    if request.method == 'POST':
+        consultation.delete()
+        return redirect('consultation_list')
+    return render(request, 'consultation/consultation_confirm_delete.html', {'consultation': consultation})
+
