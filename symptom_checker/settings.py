9dda7c16ae84e06d96c07acb8880143b2f652058
hanin303 <benjemaahanin@gmail.com>
2024-10-28 14:00:57 +0100

    Fix : add background images

--- symptom_checker/settings.py
+++ symptom_checker/settings.py
@@ -125,10 +125,13 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     BASE_DIR / "back_office/assets",
     BASE_DIR / "front_office/assets",
+    BASE_DIR / "public",
 ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
