017790921414cf64bca0a0b80f95690dab1cf7a9
Marwen-Neji <78695522+Marwen-Neji@users.noreply.github.com>
2024-10-28 13:18:56 +0100

    "final commit"

--- Consultation/forms.py
+++ Consultation/forms.py
@@ -4,4 +4,7 @@ from .models import Consultation
 class ConsultationForm(forms.ModelForm):
     class Meta:
         model = Consultation
-        fields = '__all__'
+        fields = ['doctor_name', 'patient_name', 'consultation_date', 'notes']
+        widgets = {
+            'consultation_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
