df4544aa30d0e2039a8c880ea2ef3a09f8dbb290
Imen Frigui <imen_frigui@outlook.com>
2024-10-28 14:57:18 +0100

    marwen

--- Consultation/summarizer.py
+++ Consultation/summarizer.py
@@ -5,3 +5,4 @@ def generate_summary(text):
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
     summary = summarizer(text, max_length=50, min_length=25, do_sample=False)
     return summary[0]["summary_text"]
+    
